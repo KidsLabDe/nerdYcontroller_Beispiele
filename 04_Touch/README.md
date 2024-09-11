0e18a06d81c4473635d0671e285f7d6bc2039fa2
KingBBQ <gw@communitylabs.de>
2024-09-11 15:54:43 +0200

    bug fix

--- 04_Touch/README.md
+++ 04_Touch/README.md
@@ -4,8 +4,3 @@
 
 https://docs.circuitpython.org/en/latest/shared-bindings/touchio/index.html#
 
-## Probleme mit 2350!
-
-https://github.com/adafruit/circuitpython/issues/9541
-
-Aktuell geht der Touch mit dem nicht.
--- 05_Neigung/neigung.py
+++ 05_Neigung/neigung.py
@@ -6,19 +6,19 @@ from adafruit_lsm6ds.lsm6ds3 import LSM6DS3
 import nerdy
 
 # Erstellen Sie eine I2C-Instanz
-i2c = busio.I2C(scl=board.GP7, sda=board.GP6)  # SCL an GP7, SDA an GP6
+#i2c = busio.I2C(scl=board.GP7, sda=board.GP6)  # SCL an GP7, SDA an GP6
 
 # Initialisieren Sie den Sensor
-sensor = LSM6DS3(i2c)
+#sensor = LSM6DS3(i2c)
 
 while True:
     # Beschleunigungsdaten lesen (x, y, z Achsen in m/s^2)
-    accel_x, accel_y, accel_z = sensor.acceleration
-    temp = sensor.temperature
+    accel_x, accel_y, accel_z = nerdy.sensor.acceleration
+    temp = nerdy.sensor.temperature
     print(f"Beschleunigung: X: {accel_x:.2f} m/s^2, Y: {accel_y:.2f} m/s^2, Z: {accel_z:.2f} m/s^2")
 
     # Gyroskopdaten lesen (x, y, z Achsen in Grad pro Sekunde)
-    gyro_x, gyro_y, gyro_z = sensor.gyro
+    gyro_x, gyro_y, gyro_z = nerdy.sensor.gyro
     print(f"Gyroskop: X: {gyro_x:.2f} dps, Y: {gyro_y:.2f} dps, Z: {gyro_z:.2f} dps")
     
     print(f"Temperatur: {temp:.2f} Grad Celsius")
