f862e86aab2840aa6e4cef1d4a6d496a48bf79eb
KingBBQ <gw@communitylabs.de>
2024-09-17 13:03:37 +0200

    test für alle componenten

--- main.py
+++ main.py
@@ -1,53 +1,22 @@
 import nerdy
 import time
-import pwmio
 import board
+import pwmio
+import touchio
+
 
 # Initialisieren Sie den PWM-Ausgang für den Lautsprecher
 speaker = pwmio.PWMOut(board.GP20, duty_cycle=0, frequency=440, variable_frequency=True)
 
-# Frequenzen für die Töne
-NOTE_C5 = 523
-NOTE_E5 = 659
-NOTE_G5 = 784
-NOTE_A5 = 880
-NOTE_B5 = 988
-NOTE_C6 = 1047
-NOTE_D6 = 1175
-NOTE_E6 = 1319
-NOTE_F6 = 1397
-NOTE_G6 = 1568
 
-# Tetris-Melodie (Korobeiniki)
-melody = [
-    NOTE_E5, NOTE_B5, NOTE_C6, NOTE_D6, NOTE_C6, NOTE_B5, NOTE_A5, NOTE_A5, 
-    NOTE_C6, NOTE_E6, NOTE_D6, NOTE_C6, NOTE_B5, NOTE_E5, NOTE_G5, NOTE_A5,
-    NOTE_F6, NOTE_A5, NOTE_G5, NOTE_F6, NOTE_E6, NOTE_C6, NOTE_E6, NOTE_D6,
-    NOTE_C6, NOTE_B5, NOTE_C6, NOTE_D6, NOTE_E6, NOTE_C6, NOTE_A5, NOTE_A5
+# Initialisieren Sie die Touch-Pins
+touch_pins = [
+    touchio.TouchIn(board.GP14),
+    touchio.TouchIn(board.GP15),
+    touchio.TouchIn(board.GP16),
+    touchio.TouchIn(board.GP17)
 ]
 
-# Notendauern (in Sekunden)
-note_durations = [
-    0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3,
-    0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3,
-    0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3,
-    0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3
-]
-
-
-
-def play_tone(frequency, duration):
-    speaker.frequency = int(frequency)
-    speaker.duty_cycle = 40000  # 50% duty cycle
-    time.sleep(duration)
-    speaker.duty_cycle = 0  # Turn off the sound
-
-def play_melody():
-    # Play the melody
-    for i in range(len(melody)):
-        play_tone(melody[i], note_durations[i])
-        time.sleep(0.05)  # Brief pause between notes
-    
 
 def chase_effect(farbe = nerdy.ROT, wartezeit = 0.1):
     """Erzeugt einen Chase-Effekt auf den Neopixel-LEDs."""
@@ -62,17 +31,36 @@ def chase_effect(farbe = nerdy.ROT, wartezeit = 0.1):
         time.sleep(wartezeit)
         nerdy.pixels[i] = nerdy.SCHWARZ  # LED ausschalten
 
+def play_tone(frequency, duration):
+    speaker.frequency = int(frequency)
+    speaker.duty_cycle = 500  # 50% duty cycle
+    time.sleep(duration)
+    speaker.duty_cycle = 0  # Turn off the sound
 
 
 #nerdY.regenbogen()
 
 
+def check_touch():
+    """Prüft die Touch-Pins und schaltet die LEDs entsprechend ein."""
+    for i, touch_pin in enumerate(touch_pins):
+        if touch_pin.value:
+            nerdy.pixels[i] = (0, 255, 0)  # Grün bei Berührung
+        else:
+            nerdy.pixels[i] = (0, 0, 0)  # Aus, wenn nicht berührt
+    nerdy.pixels.show()
+
+
 while True:
 # Beispiel: Roter Chase-Effekt mit einer Wartezeit von 0.1 Sekunden
+    play_tone(523,0.5)
+    play_tone(659,0.5)
     chase_effect()
     nerdy.regenbogen()
     chase_effect(nerdy.BLAU,0.2)
-    chase_effect(nerdy.GRÜN,0.05) # Beispiel: Grüner Chase-Effekt mit einer Wartezeit von 0.05 Sekunden
-
-    play_melody()
-    time.sleep(2) 
+    chase_effect(nerdy.GRÜN,0.05)
+    
+    for _ in range(20):
+        check_touch()
+        time.sleep(0.1)
+    
